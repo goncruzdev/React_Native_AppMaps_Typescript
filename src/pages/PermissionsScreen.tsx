728eb5fb99d6854559e832a751ac62265b4cdf8e
gonzalo cruz <cruzgonzalo.7@gmail.com>
2022-08-10 15:08:49 -0300

    agrego solicitud de permisos para dispositivos android y IOS

--- src/pages/PermissionsScreen.tsx
+++ src/pages/PermissionsScreen.tsx
@@ -1,12 +1,38 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Button, Platform} from 'react-native';
 import React from 'react';
+import {PERMISSIONS, PermissionStatus, request} from 'react-native-permissions';
 
 const PermissionsScreen = () => {
+  const checkPermissionLocation = async () => {
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === 'ios') {
+      // permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      // permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+    }
+
+    console.log(permissionStatus);
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>PermissionsScreen</Text>
+      <Button title="Permiso" onPress={checkPermissionLocation} />
     </View>
   );
 };
 
 export default PermissionsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
