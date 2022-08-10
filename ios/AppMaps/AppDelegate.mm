510ca73648246c2e2a98421d391ac6dc135a9972
gonzalo cruz <cruzgonzalo.7@gmail.com>
2022-08-10 18:42:09 -0300

    agrego libreria y configuracion de google maps

--- ios/AppMaps/AppDelegate.mm
+++ ios/AppMaps/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -31,6 +33,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDrryGBnHGXcwaDmVrEhZ2ENOlc8t7fA0Y"]; // add this line using the api key obtained from Google Console
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
